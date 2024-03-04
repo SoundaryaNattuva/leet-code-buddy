3aa4118d50cc02adeb5edb64bdb5bf59ac25e81e
SoundaryaNattuva <soundaryanatts@gmail.com>
2024-03-03 20:58:03 -0500

    create update and delete functions and templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('applications/', views.app_index, name = 'app-index'),
   path('applications/<int:app_id>/', views.app_detail, name = 'app-detail'),
-  path('applications/create', views.AppCreate.as_view(), name='app-create'),
-  path('applications/<int:pk>/update', views.AppUpdate.as_view(), name='app-update'),
-  path('applications/<int:pk>/delete', views.AppDelete.as_view(), name='app-delete'),
+  path('applications/create/', views.AppCreate.as_view(), name='app-create'),
+  path('applications/<int:pk>/update/', views.AppUpdate.as_view(), name='app-update'),
+  path('applications/<int:pk>/delete/', views.AppDelete.as_view(), name='app-delete'),
 ]
