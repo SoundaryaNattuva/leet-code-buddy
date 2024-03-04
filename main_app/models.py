8fa4b64cf0843e822e753493cc2cfbf6b96237e2
SoundaryaNattuva <soundaryanatts@gmail.com>
2024-03-03 20:37:44 -0500

    create new application form that allows user to create and update their app

--- main_app/models.py
+++ main_app/models.py
@@ -4,9 +4,9 @@ from datetime import date
 from django.contrib.auth.models import User
 
 SMILES = (
-  ('1', '😐'), #neutral
-  ('2', '😃'), #happy
-  ('3', '😍'), #very excited
+  ('1', '😐 Neutral'), #neutral
+  ('2', '😃 Happy '), #happy
+  ('3', '😍 Excited'), #very excited
 )
 
 ARRANGEMENTS = (
@@ -25,21 +25,76 @@ STATUS = (
   ('R', 'Rejected'),
 )
 
+STATES = (
+  ('AL', 'Alabama'),
+  ('AK', 'Alaska'),
+  ('AZ', 'Arizona'),
+  ('AR', 'Arkansas'),
+  ('CA', 'California'),
+  ('CO', 'Colorado'),
+  ('CT', 'Connecticut'),
+  ('DE', 'Delaware'),
+  ('FL', 'Florida'),
+  ('GA', 'Georgia'),
+  ('HI', 'Hawaii'),
+  ('ID', 'Idaho'),
+  ('IL', 'Illinois'),
+  ('IN', 'Indiana'),
+  ('IA', 'Iowa'),
+  ('KS', 'Kansas'),
+  ('KY', 'Kentucky'),
+  ('LA', 'Louisiana'),
+  ('ME', 'Maine'),
+  ('MD', 'Maryland'),
+  ('MA', 'Massachusetts'),
+  ('MI', 'Michigan'),
+  ('MN', 'Minnesota'),
+  ('MS', 'Mississippi'),
+  ('MO', 'Missouri'),
+  ('MT', 'Montana'),
+  ('NE', 'Nebraska'),
+  ('NV', 'Nevada'),
+  ('NH', 'New Hampshire'),
+  ('NJ', 'New Jersey'),
+  ('NM', 'New Mexico'),
+  ('NY', 'New York'),
+  ('NC', 'North Carolina'),
+  ('ND', 'North Dakota'),
+  ('OH', 'Ohio'),
+  ('OK', 'Oklahoma'),
+  ('OR', 'Oregon'),
+  ('PA', 'Pennsylvania'),
+  ('RI', 'Rhode Island'),
+  ('SC', 'South Carolina'),
+  ('SD', 'South Dakota'),
+  ('TN', 'Tennessee'),
+  ('TX', 'Texas'),
+  ('UT', 'Utah'),
+  ('VT', 'Vermont'),
+  ('VA', 'Virginia'),
+  ('WA', 'Washington'),
+  ('WV', 'West Virginia'),
+  ('WI', 'Wisconsin'),
+  ('WY', 'Wyoming')
+)
 
 # Create your models here.
 class Application(models.Model):
-  date = models.DateField('Application Submission Date')
+  date = models.DateField('Application Submission Date', null=False, blank=True)
   position = models.CharField(max_length=100)
   company = models.CharField(max_length=100)
   enthusiasm = models.CharField(max_length=1, choices=SMILES, default=SMILES[0][0] )
-  workArrangement = models.CharField(max_length=1, choices=ARRANGEMENTS, default=ARRANGEMENTS[0][0])
-  location = models.CharField(max_length=50)
-  techstack = models.TextField(max_length=100)
+  workArrangement = models.CharField('Work Arrangement', max_length=1, choices=ARRANGEMENTS, default=ARRANGEMENTS[0][0])
+  state = models.CharField(max_length=2, choices=STATES, default=STATES[0][0])
+  city = models.CharField(max_length=50)
+  techstack = models.TextField('Technology Stack', max_length=100)
   status = models.CharField(max_length=1, choices=STATUS, default=STATUS[0][0])
-  minsalary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-  maxsalary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-  notes = models.TextField(max_length=500)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  minsalary = models.DecimalField('Minimum Salary',max_digits=10, decimal_places=2, null=True, blank=True)
+  maxsalary = models.DecimalField('Maximum Salary', max_digits=10, decimal_places=2, null=True, blank=True)
+  notes = models.TextField(max_length=1000)
 
   def __str__(self):
     return self.position
+  
+  def get_absolute_url(self):
+    return reverse('app-detail', kwargs={'app_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Application
 
 # Create your views here.
 def home(request):
@@ -8,4 +10,22 @@ def about(request):
   return render(request, 'about.html')
 
 def app_index(request):
+  apps = Application.objects.all()
   return render(request, 'applications/index.html', { 'apps': apps })
+
+def app_detail(request, app_id):
+  app = Application.objects.get(id=app_id)
+  return render(request, 'applications/detail.html', {'app' : app} )
+
+class AppCreate(CreateView):
+  model = Application
+  fields = ['date', 'position', 'company', 'enthusiasm', 'workArrangement', 'state', 'city', 'techstack', 'status', 'minsalary', 'maxsalary', 'notes']
+  success_url = '/applications'
+
+class AppUpdate(UpdateView):
+  model = Application
+  fields = ['date', 'position', 'company', 'enthusiasm', 'workArrangement', 'state', 'city', 'techstack', 'status', 'minsalary', 'maxsalary', 'notes']
+
+class AppDelete(DeleteView):
+  model = Application
+  success_url = '/applications/'
